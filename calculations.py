e890fe3ed4c42a1550dbcdc7309572fd0653ddfc
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-05-29 17:01:34 +0530

    Fixed

---
+++ calculations.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+values = [43, 54, 23, 21, 19, 10, 29]
+
+np_values = np.array(values)
+
+print(np_values)
+
+to_multiply = 1.2943
+
+np_value_new = np_values * to_multiply
+
+print(np_value_new)
